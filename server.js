0ecf24bfda6b46063427158dcade78f1e57f85b8
Dennis <denniskyn80@gmail.com>
2020-04-14 01:38:38 +0300

    log fix

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ const app = express();
 const logs = fs.createWriteStream(path.join(__dirname, 'logs.log'), { flags: 'a' })
 app.use(express.json())
 app.use(xmlMiddleware())
-app.use(morgan(':method \t\t :url \t\t :status \t\t :response-time ms', { stream: logs }))
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),'\t\t',
+      tokens.url(req, res),'\t\t',
+      tokens.status(req, res),'\t\t',
+      tokens.res(req, res, 'content-length'),'\t\t', 
+      Math.trunc(tokens['response-time'](req, res))+'ms',
+    ].join(' ')
+  }, { stream: logs }))
 const port =  process.env.PORT || 5000;
 app.get('/',(req,res)=>{
     res.send('Hello world');
-})
+});
 app.use('/api/v1/on-covid-19',covidRouter)
 app.listen(port,() => {
     console.log(`Server is running on ${port}`)
